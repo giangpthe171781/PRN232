a0366e39d90bd5280edc717c9c9624a1252c8ff4
Zang <giangpthe171781@fpt.edu.vn>
2025-05-16 16:10:06 +0700

    first commit

---
+++ Slot2_todoItem/Slot2_todoItem/Models/TodoContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Slot2_todoItem.Models
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options) : base(options)
+        {
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+
+        // Optional nếu dùng InMemoryDatabase cho test/dev
+        protected void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+                optionsBuilder.UseInMemoryDatabase("TodoList");
+        }
+    }
+}
